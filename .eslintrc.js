b20613b8a680608e51eafaa3038868c9fc87e27f
Natsuki <natsuki-yamada@cybozu.co.jp>
2023-09-28 17:32:25 +0900

    add setting of webpack

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,4 +2,32 @@ module.exports = {
   extends: [
     '@cybozu/eslint-config/presets/typescript',
   ],
+  env: {
+    node: true,
+  },
+  plugins: ['import'],
+  rules: {
+    // define import rules
+    'import/order': [
+      'error',
+      // the order of import is sorted as follows:
+      {
+        groups: [
+          'builtin',
+          'external',
+          'internal',
+          'parent',
+          'sibling',
+          'index',
+          'object',
+          'type',
+        ],
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true,
+        },
+        'newlines-between': 'always',
+      },
+    ],
+  },
 };
--- src/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-const Sato = {
-    name: 'Sato',
-    age: 28,
-    skills: ['JavaScript', 'TypeScript', 'React', 'Vue', 'Angular'],
-};
