8c61da892a70be465486056cfba3c6adc22fae6c
Natsuki <natsuki-yamada@cybozu.co.jp>
2023-10-18 22:39:36 +0900

    change index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,5 @@
 (() => {
   kintone.events.on(["app.record.index.show"], (event) => {
     console.log(event);
-
-    const url = new URL(location.href);
-    const message = url.searchParams.get("message");
-    if (message !== null) {
-      const resultElm = document.querySelector("#result");
-      if (resultElm) resultElm.textContent = message;
-    }
   });
 })();
